e0421cda8dba89ba910f76adf1d3eed6101675e1
victor2905 <98538867+victor2905@users.noreply.github.com>
2023-02-01 18:15:39 -0500

    EjemploJson
    Creación de proyecto

---
+++ app/src/main/java/com/example/ejemplojson/Persona.kt
@@ -0,0 +1,15 @@
+package com.example.ejemplojson
+
+class Persona (nombre:String, pais:String, estado:String, experiencia:Int) {
+    var nombre:String = ""
+    var pais:String = ""
+    var estado:String = ""
+    var experiencia:Int = 0
+
+    init {
+        this.nombre = nombre
+        this.pais = pais
+        this.estado = estado
+        this.experiencia = experiencia
+    }
+}
